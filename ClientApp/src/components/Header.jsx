4a742b761021b1d57df2e2f178fbf3d5ea06542a
Abtahee Ali <abtaheeali@gmail.com>
2020-07-06 14:52:19 -0400

    added header component

---
+++ ClientApp/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function Header() {
+  return (
+    <>
+      <h1>Welcome to Suncoast Overflow.</h1>
+      <h2 className="mb-4">Helping developers one code at a time!</h2>
+    </>
+  )
+}
