bd72897a92c68a2f711f5c0d79dbc2db6675fcc4
Abtahee Ali <abtaheeali@gmail.com>
2020-07-06 15:04:25 -0400

    Added NavBar

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './custom.scss'
+import { Header } from './components/Header'
+import { NavBar } from './components/NavBar'
 
 export function App() {
-  return <p>App</p>
+  return (
+    <>
+      <NavBar />
+      <Header />
+    </>
+  )
 }
