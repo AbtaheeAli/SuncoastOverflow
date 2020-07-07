1707b1f067af2c424d35ac120dd10643e834a244
Abtahee Ali <abtaheeali@gmail.com>
2020-07-07 18:27:35 -0400

    Added seeds.sql and inserted into table

--- Models/seeds.sql
+++ Models/seeds.sql
@@ -1,7 +1,5 @@
 TRUNCATE TABLE "Questions";
 
 
-INSERT INTO "Questions" ("Title", "Body", "Tags") VALUES ('Thoughtbeat', 'Inverse zero administration benchmark', '07 Meadow Vale Drive', '314-651-9791');
-INSERT INTO "Questions" ("Title", "Body", "Tags") VALUES ('Dabtype', 'Organized stable firmware', '7 Miller Park', '523-760-6681');
-INSERT INTO "Questions" ("Title", "Body", "Tags") VALUES ('Topdrive', 'Object-based interactive application', '65 Eliot Lane', '650-993-7074');
-INSERT INTO "Questions" ("Title", "Body", "Tags") VALUES ('Avaveo', 'Persistent zero defect process improvement', '2 Clarendon Junction', '715-663-5265');
+INSERT INTO "Questions" ("Title", "Body", "Tags") VALUES ('C#', 'What is C#?', 'C# Back-end');
+INSERT INTO "Questions" ("Title", "Body", "Tags") VALUES ('ReactJS Hooks', 'What is React Hooks', 'ReactJS JavaScript Hooks', );
