85a2886fe4e701fac872e7e9b0c2444bddbeabac
Abtahee Ali <abtaheeali@gmail.com>
2020-07-08 15:13:22 -0400

    when clicking a vote, questions are now orbery by name so they do not move when vote button is clicked

--- Controllers/QuestionsController.cs
+++ Controllers/QuestionsController.cs
@@ -35,11 +35,11 @@ namespace SuncoastOverflow.Controllers
         {
             if (filter == null)
             {
-                return await _context.Questions.ToListAsync();
+                return await _context.Questions.OrderBy(question => question.Title).ToListAsync();
             }
             else
             {
-                return await _context.Questions.Where(question => question.Title.Contains(filter)).ToListAsync();
+                return await _context.Questions.Where(question => question.Title.Contains(filter)).OrderBy(Question => Question.Title).ToListAsync();
             }
         }
 
