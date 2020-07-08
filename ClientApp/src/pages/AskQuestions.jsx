75b59a9ede4193adf1e0bf8bb2fde7aca591c5c4
Abtahee Ali <abtaheeali@gmail.com>
2020-07-07 20:41:05 -0400

    added handle for asking new question

--- ClientApp/src/pages/AskQuestions.jsx
+++ ClientApp/src/pages/AskQuestions.jsx
@@ -1,32 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function AskQuestions() {
+  const [newQuestion, setNewQuestion] = useState({
+    title: '',
+    body: '',
+    tags: '',
+  })
+
+  const handleFormFieldChange = event => {
+    const whichFieldChanged = event.target.id
+    const newValue = event.target.value
+
+    setNewQuestion({
+      ...newQuestion,
+      [whichFieldChanged]: newValue,
+    })
+  }
+
   return (
     <div className="card">
       <div className="card-header">Ask a Question</div>
       <div className="card-body">
         <form>
           <div className="form-group">
-            <label htmlFor="name">Title</label>
-            <input type="text" className="form-control" id="name" />
-            <small id="descriptionHelp" className="form-text text-muted">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              value={newQuestion.title}
+              onChange={handleFormFieldChange}
+            />
+            <small id="bodyHelp" className="form-text text-muted">
               Be specific and imagine you're asking a question to another person
             </small>
           </div>
 
           <div className="form-group">
-            <label htmlFor="description">Body</label>
-            <textarea type="text" className="form-control" id="description" />
-            <small id="descriptionHelp" className="form-text text-muted">
+            <label htmlFor="body">Body</label>
+            <textarea
+              type="text"
+              className="form-control"
+              id="body"
+              value={newQuestion.body}
+              onChange={handleFormFieldChange}
+            />
+            <small id="bodyHelp" className="form-text text-muted">
               Include all the information someone would need to answer your
               question
             </small>
           </div>
 
           <div className="form-group">
-            <label htmlFor="description">Tags</label>
-            <textarea type="text" className="form-control" id="address" />
-            <small id="descriptionHelp" className="form-text text-muted">
+            <label htmlFor="tags">Tags</label>
+            <textarea
+              type="text"
+              className="form-control"
+              id="tags"
+              value={newQuestion.tags}
+              onChange={handleFormFieldChange}
+            />
+            <small id="tagsHelp" className="form-text text-muted">
               Add up to 5 tags to describe what your question is about
             </small>
           </div>
