e1f18fae5eed93308635893d753ccaf01ac15437
Abtahee Ali <abtaheeali@gmail.com>
2020-07-08 16:08:59 -0400

    removed comments

--- Models/Answer.cs
+++ Models/Answer.cs
@@ -4,10 +4,9 @@ namespace SuncoastOverflow.Models
 {
     public class Answer
     {
-        public int id { get; set; }
-        public string body { get; set; }
+        public int Id { get; set; }
+        public string Body { get; set; }
         public DateTime CreatedAt { get; private set; } = DateTime.Now;
         public int QuestionId { get; set; }
-
     }
 }
--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -7,10 +7,9 @@ namespace SuncoastOverflow.Models
 {
     public partial class DatabaseContext : DbContext
     {
-        // Change this if you want to have a different database name in development
         private static string DEVELOPMENT_DATABASE_NAME = "SuncoastOverflowDatabase";
 
-        // Change this to true if you want to have logging of SQL statements in development
+
         private static bool LOG_SQL_STATEMENTS_IN_DEVELOPMENT = false;
 
         public DbSet<Question> Questions { get; set; }
