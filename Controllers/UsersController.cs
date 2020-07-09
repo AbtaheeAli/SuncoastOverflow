09736a3402554cd3244d770f2ed706f054634f5b
Abtahee Ali <abtaheeali@gmail.com>
2020-07-09 14:48:13 -0400

    Added UsersController

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuncoastOverflow.Models;
+
+namespace SuncoastOverflow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public UsersController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+    }
+}
