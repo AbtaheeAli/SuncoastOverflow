01064dd726db890cebb1c4528d8f972b4df56831
Abtahee Ali <abtaheeali@gmail.com>
2020-07-09 13:41:06 -0400

    Fixing migrations

--- Migrations/20200707223501_CreateQuestion.cs
+++ Migrations/20200707223501_CreateQuestion.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace SuncoastOverflow.Migrations
 {
@@ -6,38 +7,26 @@ namespace SuncoastOverflow.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.RenameColumn(
-                name: "title",
-                table: "Questions",
-                newName: "Title");
+            migrationBuilder.CreateTable(
+               name: "Questions",
+               columns: table => new
+               {
+                   Id = table.Column<int>(nullable: false)
+                       .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                   Title = table.Column<string>(nullable: true),
+                   Body = table.Column<string>(nullable: true),
+                   Tags = table.Column<string>(nullable: true),
 
-            migrationBuilder.RenameColumn(
-                name: "tags",
-                table: "Questions",
-                newName: "Tags");
-
-            migrationBuilder.RenameColumn(
-                name: "body",
-                table: "Questions",
-                newName: "Body");
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_Questions", x => x.Id);
+               });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.RenameColumn(
-                name: "Title",
-                table: "Questions",
-                newName: "title");
 
-            migrationBuilder.RenameColumn(
-                name: "Tags",
-                table: "Questions",
-                newName: "tags");
-
-            migrationBuilder.RenameColumn(
-                name: "Body",
-                table: "Questions",
-                newName: "body");
         }
     }
 }
