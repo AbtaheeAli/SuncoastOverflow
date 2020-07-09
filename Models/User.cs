edb4b2cc4cf2ca58cc4aa032aab5a0072ec3b386
Abtahee Ali <abtaheeali@gmail.com>
2020-07-09 14:36:01 -0400

    removed comments and extra spacing

--- Models/User.cs
+++ Models/User.cs
@@ -13,23 +13,17 @@ namespace SuncoastOverflow.Models
         public string Email { get; set; }
         [JsonIgnore]
         public string HashedPassword { get; set; }
-        // Define a property for being able to _set_ a password
         public string Password
         {
-            // Define only the `set` aspect of the property
             set
             {
-                // When set, use the PasswordHasher to encrypt the password
-                // and store the result in our HashedPassword
                 this.HashedPassword = new PasswordHasher<User>().HashPassword(this, value);
             }
         }
-        // Add a method that can validate this user's password
         public bool IsValidPassword(string password)
         {
-            // Look to see if this password, and the user's hashed password can match
             var passwordVerification = new PasswordHasher<User>().VerifyHashedPassword(this, this.HashedPassword, password);
-            // Return True if the verification was a success
+
             return passwordVerification == PasswordVerificationResult.Success;
         }
     }
